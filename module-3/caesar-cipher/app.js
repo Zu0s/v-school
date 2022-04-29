d34fafa362fd023444418e14930dffae926cd39a
Brandon Butkovich <brandon.butk@gmail.com>
2022-04-28 20:34:25 -0700

    caser cipher is started and completed

---
+++ module-3/caesar-cipher/app.js
@@ -0,0 +1,44 @@
+const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+var readline = require('readline-sync');
+var input = readline.question('What phrase would you like to encrypt? ').toLowerCase();
+var shift = parseInt(readline.question('How many letters would you like to shift? '));
+
+// const newInput
+function encrypt() {
+    let editedInput = [...input]; // change
+    console.log(editedInput)
+
+    
+    for (var i = 0; i < editedInput.length; i++) {
+        
+        let shiftIndex = alphabet.indexOf(editedInput[i]) + shift
+        console.log(shiftIndex)
+        let shiftedLetter
+
+        if (editedInput[i] === ' ' ) {
+            shiftedLetter = ' '
+        } else {
+            // shiftIndex <= 25 ? shiftedLetter = alphabet[shiftIndex] : shiftIndex = shiftIndex - 26; shiftedLetter = alphabet[shiftIndex]
+
+            if (shiftIndex <= 25) {
+                shiftedLetter = alphabet[shiftIndex] 
+            } else {
+                while(shiftIndex > 25) {
+                    shiftIndex = shiftIndex - 26
+                }
+
+                shiftedLetter = alphabet[shiftIndex]
+            }
+        }
+
+        // console.log(`original value= ${editedInput[i]} new value= ${shiftedLetter}`) // this is just saying what moved to what. A grand key as to say
+        editedInput[i] = shiftedLetter
+    }
+
+    const newString = editedInput.join()
+    const commaRemoved = newString.replaceAll(',', '')
+
+    console.log(commaRemoved)
+}
+encrypt()
