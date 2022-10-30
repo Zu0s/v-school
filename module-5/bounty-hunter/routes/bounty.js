e71f916c36ca8bd951f875007867200d3d5a52c1
Brandon Butkovich <brandon.butk@gmail.com>
2022-10-29 19:31:17 -0700

    completed bounty hunter part 2

--- module-5/bounty-hunter/routes/bounty.js
+++ module-5/bounty-hunter/routes/bounty.js
@@ -6,24 +6,24 @@ const { v4 : uuidv4 } = require("uuid")
 
 const bounties = [
     {
-        firstName: 'Brandon',
-        lastName: 'Butkovich',
+        firstName: 'Mike',
+        lastName: 'Hawk',
         living: true,
         bountyAmount: 100000,
-        type: 'Sith',
+        type: 'Jedi',
         _id: uuidv4()
     },
     {
-        firstName: 'Jessie',
-        lastName: 'Cooksey',
+        firstName: 'Peter',
+        lastName: 'File',
         living: true,
         bountyAmount: 10000,
-        type: 'Jedi',
+        type: 'Sith',
         _id: uuidv4()
     }
 ]
 
-bountyRouter.route('/')
+bountyRouter.route('/') // guess I cant use .route because parameters wont pass over
 
 .get((req, res) => {
     res.send(bounties)
@@ -34,5 +34,20 @@ bountyRouter.route('/')
     bounties.push(req.body)
     res.send(`succesfully added a bounty on the ${req.body.type}, ${req.body.firstName} ${req.body.lastName} for ${req.body.bountyAmount}`)
 })
+
+bountyRouter.route('/:bountyId')
+
+.delete((req, res) => {
+    const bountyIndex = bounties.findIndex(bounty => bounty._id === req.params.bountyId)
+    bounties.splice(bountyIndex, 1)
+    res.send('Succesfully removed the bounty')
+})
+
+.put((req, res) => { 
+    const bountyIndex = bounties.findIndex(bounty => bounty._id === req.params.bountyId)
+    res.send(Object.assign(bounties[bountyIndex], req.body))
+})
+
+
 
 module.exports = bountyRouter
