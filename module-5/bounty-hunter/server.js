93d34da17c0a37abf978869e51dfc2a583ccbd83
Brandon Butkovich <brandon.butk@gmail.com>
2022-11-04 06:32:45 -0700

    starting to add the fron end

--- module-5/bounty-hunter/server.js
+++ module-5/bounty-hunter/server.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const app = express() 
+const morgan = require('morgan')
+
+app.use(morgan('dev'))
 app.use(express.json())
 
-// Routes / 
 app.use('/bounty', require('./routes/bounty.js'))
-
 
 app.listen(8000, () => { console.log('the server is running on port 8000')})
