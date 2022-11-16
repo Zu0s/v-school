aa967434df5368ceed37810a35e63a5114a9abac
Brandon Butkovich <brandon.butk@gmail.com>
2022-11-16 11:06:45 -0800

    delete request is working succesfuly

--- module-5/bounty-hunter/client/src/components/Bounty.js
+++ module-5/bounty-hunter/client/src/components/Bounty.js
@@ -1,14 +1,14 @@
 import React from "react"
 
 export default function Bounty(props) {
-    const { firstName, lastName, bountyAmount, type, deleteBounty} = props
+    const { firstName, lastName, bountyAmount, type, deleteBounty, _id} = props
     
     return(
         <div className="bounty">
             <h1>{firstName} {lastName}</h1>
             <h2>{bountyAmount}</h2>
             <h2>{type}</h2>
-            <button onClick={() => {deleteBounty()}}>Delete</button>
+            <button onClick={() => {deleteBounty(_id)}}>Delete</button>
         </div>
     )
 }
