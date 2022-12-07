311c73fc596f27a7ed14710e26ebc9377036fc60
Brandon Butkovich <brandon.butk@gmail.com>
2022-12-07 04:51:22 -0800

    changed pre made database to mongodb

--- module-5/bounty-hunter/server.js
+++ module-5/bounty-hunter/server.js
@@ -1,10 +1,22 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express() 
 const morgan = require('morgan')
 
 app.use(morgan('dev'))
 app.use(express.json())
 
+mongoose.set('strictQuery', false)
+mongoose.connect('process.env.MONGODB_URI',  
+    () => console.log("Conected to the DB")
+)
+
+
 app.use('/bounties', require('./routes/bounties.js'))
+
+app.use((err, req, res, next) => {
+    console.log(err)
+    return res.send({ errMsg: err.message })
+})
 
 app.listen(3001, () => { console.log('the server is running on port 3001')})
